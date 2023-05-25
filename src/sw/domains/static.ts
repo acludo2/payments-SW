f89eb3ed80f11d0c8ca71100464ef3b5137c38d3
cesar angulo <cesarangulo@cesars-MacBook-Air-2.local>
2023-05-24 18:29:43 -0700

    pointing canister id to domain

--- src/sw/domains/static.ts
+++ src/sw/domains/static.ts
@@ -11,5 +11,6 @@ export const hostnameCanisterIdMap: Map<string, Principal> = new Map(
     'dscvr.one': Principal.from('h5aet-waaaa-aaaab-qaamq-cai'),
     'dscvr.ic0.app': Principal.from('h5aet-waaaa-aaaab-qaamq-cai'),
     'personhood.ic0.app': Principal.from('g3wsl-eqaaa-aaaan-aaaaa-cai'),
+    'payments-sw.pages.dev':  Principal.from('sxfbn-dyaaa-aaaap-qbgca-ca'),
   })
 );
